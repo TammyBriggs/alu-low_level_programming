ee5a462af21944531ac03bb1a4d72762225bac83
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2025-09-11 04:26:51 +0200

    checking if betty errors have been fixed

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 48; i <= 57; i++)
-    {
-        putchar(i);
-        if (i == 57)
-        {
-            break;
-        }
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			break;
+		}
 
-        putchar(',');
-        putchar(' ');
-    }
-    putchar('\n');
-    return (0);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
 }
